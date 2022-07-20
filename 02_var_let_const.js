e2a558326b58847ea4f68efa4b9b76f9e2950f08
Mohit <info.mohit728@gmail.com>
2022-07-20 21:47:35 +0530

    var let and const

---
+++ 02_var_let_const.js
@@ -0,0 +1,17 @@
+var a = 45
+const b = "Mohit"
+let c = null
+let d = undefined
+console.log(a)
+{
+  let b = "this"
+  console.log(b)
+}
+console.log(b)
+console.log(c)
+console.log(d)
+
+c = 41
+console.log(c)
+b = "rohit" //Throw an error
+console.log(b)
