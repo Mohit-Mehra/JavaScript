97e8d461285ddcda3b5cfd19695ffbabe1cca4dd
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-07-21 21:45:48 +0530

    datatypes

---
+++ 03_primitives_objects.js
@@ -0,0 +1,15 @@
+let a = null;
+let b = 455;
+let c = "Mohit"
+let d = true;
+let e = BigInt("5620");
+let f = undefined;
+let g = Symbol("Hello");
+console.log(a,b,c,d,e,f,g)
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(typeof g)
