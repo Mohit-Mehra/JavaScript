59342f24207dd3b4364437088dab5827f96aeca7
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-07-24 22:51:27 +0530

    operators

---
+++ 05_Operators.js
@@ -0,0 +1,54 @@
+// 45;
+// "Mohit"
+// false
+
+//Arithmetic operators
+let a = 15;
+let b = 16;
+console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a **b = ",a**b);
+console.log("a % b = ",a%b);
+console.log("a++ = ",a++);
+console.log("a = ",a);
+console.log("a-- = ",a--);
+console.log("a = ",a);
+console.log("++a = ",++a);
+console.log("a = ",a);
+
+//Assignment Operators
+let c = 2;
+c += 10;
+console.log(c);
+
+c -=10;
+console.log(c);
+
+c *=10;
+console.log(c);
+c /=10;
+console.log(c);
+
+c %=10;
+console.log(c);
+
+c **=10;
+console.log(c);
+
+
+// Comparison opertaors
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is ",comp1 ==  comp2);
+console.log("comp1 != comp2 is ",comp1 !=  comp2);
+console.log("comp1 === comp2 is ",comp1 ===  comp2);
+console.log("comp1 !== comp2 is ",comp1 !==  comp2);
+
+// Logical operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5);
+console.log(x>y || x==5);
+console.log(!false);
