1513c6386ada6ebc2d579a9f3ca722778148391b
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-04 15:02:32 +0530

    While Loops

---
+++ 09_while_loops.js
@@ -0,0 +1,17 @@
+// While Loop
+let n = prompt("Enter a Number ")
+n = Number.parseInt(n)
+let i = 0
+while(i<n){
+  console.log(i)
+  i++
+}
+
+//do while loop
+let a = prompt("Enter a Number ")
+a = Number.parseInt(a)
+let j = 0
+do{
+  console.log(j)
+  j++
+}while(j < a)
