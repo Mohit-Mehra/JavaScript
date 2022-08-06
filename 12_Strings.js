97350ef400d7b57a228c309c6c1d604d7a6c3110
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-06 12:38:55 +0530

    Strings

---
+++ 12_Strings.js
@@ -0,0 +1,17 @@
+let name = "Mohit"
+console.log(name.length)
+console.log(name[0])
+
+let friend = 'Rohit'
+console.log(friend)
+
+// Template Litrals
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+let sentance = `${boy1} is a friend of ${boy2}`
+console.log(sentance)
+
+
+// Escape Sequence Characters
+let fruit = "Bana\'na"
+console.log(fruit)
