adb77ee307d9814a05f40193fe2b1c2be2e6f47a
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-06 12:58:11 +0530

    String Methods

---
+++ 13_String_Methods.js
@@ -0,0 +1,21 @@
+let name = "Mohit"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,6))
+console.log(name.slice(1))
+
+console.log(name.replace("M","R"))
+
+let surname = "Mehra"
+let FullName = name.concat(" ",surname)
+console.log(FullName)
+
+let a = "           Mougui jiugiu            iugiu giiug0         "
+console.log(a.trim()) // Remove White Spaces from staring and ending not from inbetween
+
+let b = "Shubham"
+for(let i of b){
+    console.log(i)
+}
+
