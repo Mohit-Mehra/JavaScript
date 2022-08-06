b31a4406241e40e98f6a94f6a7430862b1612e23
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-06 15:41:44 +0530

    Arrays

---
+++ 15_Arrays.js
@@ -0,0 +1,16 @@
+let marks = [80,90,54,42,100]
+console.log(marks)
+
+let student1 = ["Mohit",90,"Javascript"]
+console.log(student1)
+
+console.log(student1[0])
+console.log(student1[1])
+console.log(student1[2])
+console.log(student1[6]) // undefined
+
+console.log(student1.length)
+
+student1[0] = "Rohit"
+console.log(student1)
+console.log(typeof student1) //Object
