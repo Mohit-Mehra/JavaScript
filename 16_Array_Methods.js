8204196ce2aa34bd1c34a3c32471f03acdfe4143
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-08 19:08:43 +0530

    Array Methods

---
+++ 16_Array_Methods.js
@@ -0,0 +1,20 @@
+let n = [10, 25, 12, 2]
+
+let b = n.toString()
+console.log(b)
+
+let c = n.join(" and ")
+console.log(c)
+
+let d = n.pop() //Pop return the pop value
+console.log(n,d)
+
+let e = n.push(100) //Push returns the new array length
+console.log(n,e)
+
+e = n.shift() //Remove the element from the start 
+console.log(e,n)
+
+e = n.unshift(78) //returns the new array length
+console.log(e,n)
+
