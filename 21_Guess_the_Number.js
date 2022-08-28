ec205c577eb8afb16ad1fc4c729843056f070c00
Mohit-Mehra <80165502+Mohit-Mehra@users.noreply.github.com>
2022-08-28 21:09:22 +0530

    28-08-2022

---
+++ 21_Guess_the_Number.js
@@ -0,0 +1,5 @@
+// Write a program to generate a random number and store it in a variable. The program then takes an input from
+// the user to tell them whether the guess was correct, greater or lesser than the original number.
+// 100 - (no. of guesses) is the score of the user. The Program is expected to terminate once the number is 
+// guessed. Number should br between 1 - 100.
+
---
+++ 22_console_object.js
@@ -0,0 +1,8 @@
+console.log(console)
+console.error("Error")
+console.assert(5>53)
+// console.clear()
+a = {a:1,b:2,c:5}
+console.table(a)
+console.warn("hello")
+console.info("world")
